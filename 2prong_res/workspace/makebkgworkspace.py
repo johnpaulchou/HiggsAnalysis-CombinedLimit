1c4210ea996fe2753f4c27d49553173868282314
John Paul Chou <john.paul.chou@cern.ch>
2024-10-12 15:39:14 -0400

    changes

--- 2prong_res/workspace/makebkgworkspace.py
+++ 2prong_res/workspace/makebkgworkspace.py
@@ -1,75 +1,94 @@
 import ROOT
 
+# whether or not to do the sideband region instead of the signal region
+doSideband = True
 
 # where to write things out
 fileoutname = "../output/bkgworkspace.root"
 
-# get the 2D histogram from the file
-#rootfile = ROOT.TFile("../input/egamma2018-full-newbinning_signalreigon_phi_by_omega.root","READ")
-#rootfile = ROOT.TFile("../input/fullsum_Multigetsideband_sept24-HistoMode.root","READ")
-rootfile = ROOT.TFile("../input/fullsum_Multigetsideband_sept24-HistoMode_update.root","READ")
-datahist2d = rootfile.Get("plots/recomass_2d_variable_sb_tightpho")
-datahist2d.SetDirectory(0)
+# where to read from
+fileinname = "../input/egamma18.root"
 
-m2pg = ROOT.RooRealVar("m2pg","Invariant mass of the 2-prong and photon",datahist2d.GetYaxis().GetBinLowEdge(1),datahist2d.GetYaxis().GetBinUpEdge(datahist2d.GetYaxis().GetNbins()))
-#m2pg = ROOT.RooRealVar("m2pg","Invariant mass of the 2-prong and photon",700,datahist2d.GetYaxis().GetBinUpEdge(datahist2d.GetYaxis().GetNbins()))
-bins = range(1,datahist2d.GetXaxis().GetNbins()+1)
+# set up eta bins
+etabins = ["barrel","endcap"]
 
+# setup observable
+m2pg = ROOT.RooRealVar("m2pg","Invariant mass of the 2-prong and photon",500,4020)
+bins = range(1,26)
+
+# get the 2D histogram by etabin
+def get2dHist(etabin):
+    if doSideband: histname="plots/recomass_sideband_"+etabin
+    else: histname="plots/recomass_"+etabin
+    rootfile = ROOT.TFile(fileinname, "READ")
+    datahist2d = rootfile.Get(histname)
+    datahist2d.SetDirectory(0)
+    return datahist2d
+
+    
 if __name__ == "__main__":
     # open the file and create the workspace
     fileout = ROOT.TFile(fileoutname, "RECREATE")
     w = ROOT.RooWorkspace("w","w")
 
-    # loop over the 2-prong mass slices
-    for bin in range(1,datahist2d.GetXaxis().GetNbins()+1):
-        label = "bin"+str(bin)
-        datahist1d = datahist2d.ProjectionY("_py"+label,bin,bin)
+    # loop over eta bins
+    for etabin in etabins:
 
-        # convert histogram into a RooDataHist
-        dataHist = ROOT.RooDataHist("dataHist_"+label, "dataHist", m2pg, datahist1d)
+        # get the 2d data histogram
+        datahist2d = get2dHist(etabin)
+        
+        # loop over the 2-prong mass slices
+        for bin in range(1,datahist2d.GetXaxis().GetNbins()+1):
+            label = "bin"+str(bin)
+            if etabin=="barrel": label = label + "B"
+            elif etabin=="endcap": label = label + "E"
+            else: exit(1)
+            datahist1d = datahist2d.ProjectionY("_py"+label,bin,bin)
 
-        # set up the three background function models
-        p1 = ROOT.RooRealVar("p1_"+label,"p1",-5,-100,0)
-        p2 = ROOT.RooRealVar("p2_"+label,"p2",-5,-100,0)
-        sqrts = ROOT.RooRealVar("sqrts","sqrts",13000.)
-        f1 = ROOT.RooDijet1Pdf("model_bkg_f1_"+label,"f1",m2pg,p1,p2,sqrts)
-        f1.fitTo(dataHist, ROOT.RooFit.Minimizer("Minuit2","minimize"), ROOT.RooFit.SumW2Error(True), ROOT.RooFit.PrintLevel(-1))
+            # convert histogram into a RooDataHist
+            dataHist = ROOT.RooDataHist("dataHist_"+label, "dataHist", m2pg, datahist1d)
 
-        p3 = ROOT.RooRealVar("p3_"+label,"p3",-5,-100,0)
-        p4 = ROOT.RooRealVar("p4_"+label,"p4",-5,-100,0)
-        f2 = ROOT.RooDijet2Pdf("model_bkg_f2_"+label,"f2",m2pg,p3,p4,sqrts)
-        f2.fitTo(dataHist, ROOT.RooFit.Minimizer("Minuit2","minimize"), ROOT.RooFit.SumW2Error(True), ROOT.RooFit.PrintLevel(-1))
+            # set up the three background function models
+            p1 = ROOT.RooRealVar("p1_"+label,"p1",-5,-100,0)
+            p2 = ROOT.RooRealVar("p2_"+label,"p2",-5,-100,0)
+            sqrts = ROOT.RooRealVar("sqrts","sqrts",13000.)
+            f1 = ROOT.RooDijet1Pdf("model_bkg_f1_"+label,"f1",m2pg,p1,p2,sqrts)
+            f1.fitTo(dataHist, ROOT.RooFit.Minimizer("Minuit2","minimize"), ROOT.RooFit.SumW2Error(True), ROOT.RooFit.PrintLevel(-1))
 
-        p5 = ROOT.RooRealVar("p5_"+label,"p5",5,0,100)
-        p6 = ROOT.RooRealVar("p6_"+label,"p6",-5,-100,100)
-        f3 = ROOT.RooDijet3Pdf("model_bkg_f3_"+label,"f3",m2pg,p5,p6,sqrts)
-        f3.fitTo(dataHist, ROOT.RooFit.Minimizer("Minuit2","minimize"), ROOT.RooFit.SumW2Error(True), ROOT.RooFit.PrintLevel(-1))
+            p3 = ROOT.RooRealVar("p3_"+label,"p3",-5,-100,0)
+            p4 = ROOT.RooRealVar("p4_"+label,"p4",-5,-100,0)
+            f2 = ROOT.RooDijet2Pdf("model_bkg_f2_"+label,"f2",m2pg,p3,p4,sqrts)
+            f2.fitTo(dataHist, ROOT.RooFit.Minimizer("Minuit2","minimize"), ROOT.RooFit.SumW2Error(True), ROOT.RooFit.PrintLevel(-1))
 
-        # create multiPDF
-        cat = ROOT.RooCategory("pdfindex_"+label,
-                               "Index of Pdf which is active"
-        )
-        models = ROOT.RooArgList()
-        models.add(f1)
-        models.add(f2)
-        models.add(f3)
-        multipdf = ROOT.RooMultiPdf("multipdf_"+label, "MultiPdf", cat, models)
-        # naming convention of the normalization is specific to Combine and needs to be used this way for an extended likelihood. Bleh.
-        norm = ROOT.RooRealVar("multipdf_"+label+"_norm", "Number of background events", dataHist.numEntries(),0,3*dataHist.numEntries())
-        #    norm = ROOT.RooRealVar("model_bkg_f1_"+label+"_norm", "Number of background events", dataHist.numEntries(),0,3*dataHist.numEntries())
+            p5 = ROOT.RooRealVar("p5_"+label,"p5",5,0,100)
+            p6 = ROOT.RooRealVar("p6_"+label,"p6",-5,-100,100)
+            f3 = ROOT.RooDijet3Pdf("model_bkg_f3_"+label,"f3",m2pg,p5,p6,sqrts)
+            f3.fitTo(dataHist, ROOT.RooFit.Minimizer("Minuit2","minimize"), ROOT.RooFit.SumW2Error(True), ROOT.RooFit.PrintLevel(-1))
 
-        # write to workspace
-        getattr(w, "import")(dataHist)
-        getattr(w, "import")(cat)
-        getattr(w, "import")(norm)
-        getattr(w, "import")(multipdf)
+            # create multiPDF
+            cat = ROOT.RooCategory("pdfindex_"+label,
+                                   "Index of Pdf which is active"
+                                   )
+            models = ROOT.RooArgList()
+            models.add(f1)
+            models.add(f2)
+            models.add(f3)
+            multipdf = ROOT.RooMultiPdf("multipdf_"+label, "MultiPdf", cat, models)
+            # naming convention of the normalization is specific to Combine and needs to be used this way for an extended likelihood. Bleh.
+            norm = ROOT.RooRealVar("multipdf_"+label+"_norm", "Number of background events", dataHist.numEntries(),0,3*dataHist.numEntries())
 
-        # plot fits for inspection later
-        fileout.cd()
-        dataHist.Write()
-        f1.Write()
-        f2.Write()
-        f3.Write()
+            # write to workspace
+            getattr(w, "import")(dataHist)
+            getattr(w, "import")(cat)
+            getattr(w, "import")(norm)
+            getattr(w, "import")(multipdf)
+
+            # plot fits for inspection later
+            fileout.cd()
+            dataHist.Write()
+            f1.Write()
+            f2.Write()
+            f3.Write()
 
     w.Print()
     fileout.cd()
--- 2prong_res/workspace/makesigworkspace.py
+++ 2prong_res/workspace/makesigworkspace.py
@@ -1,151 +1,65 @@
 import ROOT
-from scipy.interpolate import interp1d
+import sys
 
-def get2DHist(newname,filename, histname,x,y):
+# function to get a TH1 from a ROOT file
+def getTH1(filename,histname):
     rootfile = ROOT.TFile(filename, "READ")
     if not rootfile or rootfile.IsZombie():
         print("Error: Unable to open file '{file_name}'.")
         return None
 
-    sighist = rootfile.Get(histname)
-    if not sighist or not isinstance(sighist, ROOT.TH1) or sighist is None:
-        print("Error: Histogram '{histname}' not found or is not a valid TH2 object in the file '{filename}'.")
+    hist = rootfile.Get(histname)
+    if not hist or not isinstance(hist, ROOT.TH1) or hist is None:
+        print("Error: Histogram '"+histname+"' not found or is not a valid TH1 object in the file '"+filename+"'.")
         rootfile.Close()
         return None
-    #    sighist.Scale(sighist2d.GetEntries()/sighist2d.Integral(0,sighist2d.GetXaxis().GetNbins()+1,0,sighist2d.GetYaxis().GetNbins()))
-    #    sighist.Sumw2(False)
-    sighist.SetDirectory(0)
+
+    hist.SetDirectory(0)
     rootfile.Close()
+    return hist
 
-    datahist=ROOT.RooDataHist(newname,"2D signal histogram",ROOT.RooArgList(x,y),sighist)
-    return datahist
-#    histpdf=ROOT.RooHistPdf(newname,"2d signal histogram PDF",ROOT.RooArgSet(x,y),datahist)
-#    print(histpdf)
-#    return histpdf
+# list of systematics
+systs = [ "" ]
 
-# set the omega mass and phi mass values here
-pmass=1050.
-wmass=0.50
+# determine the w and p masses from the arguments
+wmass=float(sys.argv[1])
+pmass=float(sys.argv[2])
 tlabel=str(pmass)+"_"+str(wmass)
 
-# set the signal interpolation parameters here
-txmin=0.5; txmax=2.1; tymin=450.; tymax=1050.
-tx=ROOT.RooRealVar("tx","tx",txmin,txmax)
-ty=ROOT.RooRealVar("ty","ty",tymin,tymax)
-tx.setVal(wmass)
-ty.setVal(pmass)
-fnA="input/mass2D_Phi_450_omega_0p5.root"
-fnB="input/mass2D_Phi_450_omega_2p1.root"
-fnC="input/mass2D_Phi_1050_omega_0p5.root"
-fnD="input/mass2D_Phi_1050_omega_2p1.root"
 
-# create the observables (do this based on the data's histogram boundaries)
-rootfile = ROOT.TFile("input/egamma2018-full-newbinning_signalreigon_phi_by_omega.root","READ")
-datahist2d = rootfile.Get("plots/recomass_2d_variable")
-m2p = ROOT.RooRealVar("m2p","Invariant mass of the 2-prong",datahist2d.GetXaxis().GetBinLowEdge(1),datahist2d.GetXaxis().GetBinUpEdge(datahist2d.GetXaxis().GetNbins()))
-m2pg = ROOT.RooRealVar("m2pg","Invariant mass of the 2-prong and photon",datahist2d.GetYaxis().GetBinLowEdge(1),datahist2d.GetYaxis().GetBinUpEdge(datahist2d.GetYaxis().GetNbins()))
+###### main function ######
+if __name__ == "__main__":
 
+    # loop over systematic sources
+    for syst in systs:
 
-# where to write things out
-fileout = ROOT.TFile("sigworkspace_"+tlabel+".root", "RECREATE")
-w = ROOT.RooWorkspace("w","w")
+        morphhist=getTH1("sigshapes_"+tlabel+".root", )
+    
+            # create PDFs for different m2p slices
+            fileout.cd()
+            for bin in range(1,morphhist.GetXaxis().GetNbins()+1):
+                label = "bin"+str(bin)
+                if histindex==0: label = label+"B"
+                else: label = label+"E"
+                projy=morphhist.ProjectionY("_py"+label,bin,bin)
+                accnum = projy.Integral(1,projy.GetXaxis().GetNbins())
+                accden = morphhist.Integral(1,morphhist.GetXaxis().GetNbins(),1,morphhist.GetYaxis().GetNbins())
+                dh=ROOT.RooDataHist("dh"+label,"dh"+label,m2pg,projy)
+                sigpdf1d = ROOT.RooHistPdf("sigpdf_"+label,"signal PDF for a slice in m2p",m2pg,dh)
+                sliceacc = ROOT.RooRealVar("sliceacc_"+label,"acceptance in a given slice",accnum/accden)
+                sliceacc.setConstant(True)
+                print("The slice acceptance for "+label+" is "+str(sliceacc.getValV()))
+                norm = ROOT.RooProduct("signal_"+label+"_norm", "Normalisation of signal", ROOT.RooArgList(xsec,acceff,sliceacc))
+                getattr(w,"import")(sigpdf1d)
+                getattr(w,"import")(norm)
 
-# interpolate the cross section
-theory_xs = [(450., 585.983), (500., 353.898), (625., 117.508), (750., 45.9397), (875., 20.1308),
-             (1000., 9.59447), (1125., 4.88278), (1250., 2.61745), (1375., 1.46371),
-             (1500., 0.847454), (1625., 0.505322), (1750., 0.309008), (1875., 0.192939),
-             (2000., 0.122826), (2125., 0.0795248), (2250., 0.0522742), (2375., 0.0348093),
-             (2500., 0.0235639), (2625., 0.0161926), (2750., 0.0109283), (2875., 0.00759881)]
-x=list(zip(*theory_xs))
-interp=interp1d(x[0], x[1])
-xsec=ROOT.RooRealVar("xsec","Interpolated theory x-section",interp(pmass))
-xsec.setConstant(True)
-print("The interpolated cross section is "+str(xsec.getValV())+" fb.")
+            morphhist.Write()
+            hA.Write(hA.GetName()+"A")
+            hB.Write(hB.GetName()+"B")
+            hC.Write(hC.GetName()+"C")
+            hD.Write(hD.GetName()+"D")
 
-# interpolate the acceptance*efficiency
-# this is a dummy value for now
-acceff=ROOT.RooRealVar("acceff","interpolated acceptance*efficiency",0.0001)
-acceff.setConstant(True)
-print("The interpolated acc*eff is "+str(acceff.getValV()))
-
-# interpolate the signal PDF
-bintx = ROOT.RooBinning(1,txmin,txmax)
-binty = ROOT.RooBinning(1,tymin,tymax)
-grid = ROOT.RooMomentMorphFuncNDCorr.Grid2(bintx,binty)
-histA = get2DHist("histA",fnA,"hist_sum_1",m2p,m2pg)
-histB = get2DHist("histB",fnB,"hist_sum_1",m2p,m2pg)
-histC = get2DHist("histC",fnC,"hist_sum_1",m2p,m2pg)
-histD = get2DHist("histD",fnD,"hist_sum_1",m2p,m2pg)
-histpdfA=ROOT.RooHistPdf("histpdfA","histpdfA",ROOT.RooArgSet(m2p,m2pg),histA)
-histpdfB=ROOT.RooHistPdf("histpdfB","histpdfA",ROOT.RooArgSet(m2p,m2pg),histB)
-histpdfC=ROOT.RooHistPdf("histpdfC","histpdfA",ROOT.RooArgSet(m2p,m2pg),histC)
-histpdfD=ROOT.RooHistPdf("histpdfD","histpdfA",ROOT.RooArgSet(m2p,m2pg),histD)
-grid.addPdf(histpdfA,0,0)
-grid.addPdf(histpdfB,0,1)
-grid.addPdf(histpdfC,1,0)
-grid.addPdf(histpdfD,1,1)
-morph = ROOT.RooMomentMorphFuncNDCorr("morph","morph", ROOT.RooArgList(tx,ty), ROOT.RooArgList(m2p, m2pg), grid, ROOT.RooMomentMorphFuncNDCorr.Linear)
-morph.setPdfMode()
-sighist = morph.createHistogram("m2p:m2pg")
-fileout.cd()
-sighist.Write()
-#getattr(w,"import")(sighist)
-
-#sigpdf = ROOT.RooWrapperPdf("sigpdf","sigpdf",morph)
-#print(sigpdf)
-
-#sigpdf1d = sigpdf.createProjection(ROOT.RooArgSet(m2p))
-#can = ROOT.TCanvas()
-#plot = m2pg.frame()
-#sigpdf1d.plotOn(plot)
-#plot.Draw()
-#can.Update()
-#can.Draw()
-#can.SaveAs("asdf.pdf")
-
-# create PDFs for different 2-prong mass slices
-#for bin in range(1,datahist2d.GetXaxis().GetNbins()+1):
-#    label = "bin"+str(bin)
-#    m2p.setRange("range_"+label,datahist2d.GetXaxis().GetBinLowEdge(bin),datahist2d.GetXaxis().GetBinUpEdge(bin))
-#    sigpdf1d = sigpdf.createProjection(ROOT.RooArgSet(m2p),ROOT.ProjectionRange("range_"+label))
-#    sigpdf1d = sigpdf.createProjection(ROOT.RooArgSet(m2p))
-#    sigpdf1d.SetName("signal_"+label)
-#    accnum = sigpdf.createIntegral(ROOT.RooArgSet(m2p,m2pg))
-#    dennum = sigpdf.createIntegral(ROOT.RooArgSet(m2p,m2pg))
-#    sliceacc = ROOT.RooRealVar("sliceacc_"+label,"acceptance in the given slice",accnum.getVal()/accden.getVal())
-#    sliceacc.setConstant(True)
-#    print("The slice acceptance for "+label+" is "+str(sliceacc.getValV()))
-#    norm = ROOT.RooProduct("signal_"+label+"_norm", "Normalisation of signal", ROOT.RooArgList(xsec,acceff,sliceacc))
-
-    # write to workspace
-    #getattr(w,"import")(norm)
-#    getattr(w,"import")(sigpdf1d)
-
-
-
-    # plot for inspection later
-#    can = ROOT.TCanvas()
-#    can.Divide(2,1)
-#    can.cd(1)
-#    plot1 = m2pg.frame()
-#    lo=sighist2d.GetXaxis().GetBinLowEdge(bin)
-#    up=sighist2d.GetXaxis().GetBinUpEdge(bin)
-#    plot1.SetTitle("2-prong mass ["+str(lo)+", "+str(up)+"]")
-#    sigHist.plotOn(plot1)
-#    plot1.Draw()
-#    can.cd(2)
-#    plot2 = m2pg.frame()
-#    plot2.SetTitle("2-prong mass ["+str(lo)+", "+str(up)+"]")
-#    sigHistPdf.plotOn(plot2)
-#    plot2.Draw()
-#    can.Update()
-#    can.Draw()
-#    can.SaveAs("sigplot_"+tlabel+"_"+label+".pdf")
-
-w.Print()
-fileout.cd()
-w.Write()
-fileout.Close()
-
-
-
+    w.Print()
+    w.Write()
+    fileout.Close()
+###### main function ######
