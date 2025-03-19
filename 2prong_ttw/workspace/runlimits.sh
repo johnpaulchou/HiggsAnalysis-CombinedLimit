5b72043582b944f1106ad78e14fca46bb47fa4bc
John Paul Chou <john.paul.chou@cern.ch>
2025-03-19 17:21:09 -0400

    add significance calculation to script

--- 2prong_ttw/workspace/runlimits.sh
+++ 2prong_ttw/workspace/runlimits.sh
@@ -7,5 +7,7 @@ do
     ./makenewcard.py --imass $mass
     echo "Processing mass $mass"
     combine -M AsymptoticLimits newcard.txt
+    combine -M Significance newcard.txt
     mv higgsCombineTest.AsymptoticLimits.mH120.root higgsCombineTest.AsymptoticLimits.m${mass}.root
+    mv higgsCombineTest.Significance.mH120.root higgsCombineTest.Significance.m${mass}.root
 done
