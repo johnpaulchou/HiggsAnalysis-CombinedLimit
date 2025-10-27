482d4e4a94b5608351a0944093c669d710b07149
Brandon Chiarito <bchiarito@gmail.com>
2025-10-27 17:07:05 -0400

    making func pair freezing automatic

--- 2prong/res/runbias.sh
+++ 2prong/res/runbias.sh
@@ -3,6 +3,12 @@
 source setup.sh
 ./makeconstants.py
 source constants.sh
+if [ "$debug" = "-2" ]; then
+    output="/dev/null"
+else
+    #output="/dev/fd/1"
+    output="/dev/stdout"
+fi
 
 ./makebkgworkspace.py
 ./makenewcard.py
@@ -15,7 +21,7 @@ for mass in ${masses[@]}; do
                 if [[ $genfix -eq $testfix ]]; then
                     continue
                 fi
-                echo "Processing $mass $sigstr $genfix $testfix"
+                echo "Processing $mass $sigstr $genfix $testfix" `date`
                 ./makesigworkspace.py --imass $mass
                 text2workspace.py newcard.txt
                 genfixtemp="fix$genfix"
@@ -27,8 +33,9 @@ for mass in ${masses[@]}; do
                 freeze_params2="lumi,$list_params,$freezeparams2"
                 freeze_paramstmp="freeze_params$testfix"
                 freeze_paramsstr="${!freeze_paramstmp}"
-                combine newcard.txt -M GenerateOnly --setParameters lumi=0,$genfixstr --toysNoSystematics -t $niter --expectSignal $sigstr --saveToys -m 125 --freezeParameters lumi,$list_params -v $debug
-                combine -M FitDiagnostics newcard.root --toysFile higgsCombineTest.GenerateOnly.mH125.123456.root -t $niter --rMin -$rrange --rMax $rrange --cminDefaultMinimizerStrategy=0  --X-rtd MINIMIZER_freezeDisassociatedParams --setParameters $testfixstr --freezeParameters $freeze_paramsstr -v $debug
+                
+                combine newcard.txt -M GenerateOnly --setParameters lumi=0,$genfixstr --toysNoSystematics -t $niter --expectSignal $sigstr --saveToys -m 125 --freezeParameters lumi,$list_params -v $debug > "$output"
+                combine -M FitDiagnostics newcard.root --toysFile higgsCombineTest.GenerateOnly.mH125.123456.root -t $niter --rMin -$rrange --rMax $rrange --cminDefaultMinimizerStrategy=0  --X-rtd MINIMIZER_freezeDisassociatedParams --setParameters $testfixstr --freezeParameters $freeze_paramsstr -v $debug > "$output"
 
                 mv fitDiagnosticsTest.root fitDiagnosticsTest_m${mass}_sig${sigstr}_gen${genfix}_test${testfix}.root
             done
